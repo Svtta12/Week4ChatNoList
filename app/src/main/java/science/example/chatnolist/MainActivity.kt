89ff4a1dfc14f5fc57a36bd443decff3030774e9
Svetlana <shitickowas@gmail.com>
2022-05-25 19:18:17 +0700

    Nolist
    Nolist

---
+++ app/src/main/java/science/example/chatnolist/MainActivity.kt
@@ -0,0 +1,21 @@
+package science.example.chatnolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import science.example.chatnolist.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+
+    }
